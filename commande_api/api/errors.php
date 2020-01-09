76f1e227bf610e897d41e72567e167afcaa1cdc7
sehbaoui <mouad>
2020-01-09 14:45:33 +0100

    TD2 done

---
+++ commande_api/api/errors.php
@@ -0,0 +1,32 @@
+<?php
+return $tab = ["notAllowedHandler"=>
+    $c['notAllowedHandler']= function( $c ) {
+        return function( $req, $resp , $methods ) {
+            return $resp->withStatus(405)
+                ->withHeader('Allow', implode(', ', $methods))
+                ->withHeader('Content-type', 'text/html')
+                ->write('Method must be one of: ' . implode(', ', $methods));
+        };
+    },"notFoundHandler"=>
+    $c['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $response->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('PAGE NOT FOUND !');
+    };
+},"phpErrorHandler" =>
+    $c['phpErrorHandler'] = function ($c) {
+        return function ($request, $response, $error) use ($c) {
+            return $response->withStatus(500)
+                ->withHeader('Content-Type', 'text/html')
+                ->write('Something went wrong!');
+        };
+    },"notFoundHandler"=>
+    $c['notFoundHandler']= function( $c ) {
+    return function( $req, $resp ) {
+        $resp=$resp->withStatus( 400 ) ;
+        $resp->getBody()->write( 'URI non traitée' ) ;
+        return $resp ;
+        };
+    }
+];
--- commande_api/api/index.php
+++ commande_api/api/index.php
@@ -1,10 +1,8 @@
 <?php
 require '../src/vendor/autoload.php';
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use \lbs\common\bootstrap\Eloquent;
-use \lbs\command\model\Commande as commande;
 
 $config = parse_ini_file("../src/conf/conf.ini");
 $db = new Illuminate\Database\Capsule\Manager();
@@ -12,52 +10,11 @@ $db->addConnection($config);
 $db->setAsGlobal();
 $db->bootEloquent();
 
+$errors = require 'errors.php';
 $configuration = new \Slim\Container(['settings' => ['displayErrorDetails' => true]]);
-$app = new \Slim\App($configuration);
-
-
-unset($app->getContainer()['notFoundHandler']);
-$app->getContainer()['notFoundHandler'] = function ($c) {
-    return function ($request, $response) use ($c) {
-        $response = new \Slim\Http\Response(404);
-        return $response->withHeader('Content-Type', 'application/json;charset=utf-8')
-            ->write("Page not found");
-    };
-};
-
-
-$c = $app->getContainer();
-$c['notAllowedHandler'] = function ($c) {
-    return function ($request, $response, $methods) use ($c) {
-        return $response->withStatus(405)
-            ->withHeader('Allow', implode(', ', $methods))
-            ->withHeader('Content-type', 'application/json;charset=utf-8')
-            ->write('Method must be one of: ' . implode(', ', $methods));
-    };
-};
-
-$c = $app->getContainer();
-$c['phpErrorHandler'] = function ($c) {
-    return function ($request, $response, $error) use ($c) {
-        return $response->withStatus(500)
-            ->withHeader('Content-Type', 'application/json;charset=utf-8')
-            ->write('Something went wrong! DD');
-    };
-};
+$app_config = array_merge($errors);
+$app = new \Slim\App(new \Slim\Container($app_config));
 
 $app->get('/commandes[/]', \lbs\command\control\CommandesController::class.':getCommands');
 $app->get('/commandes/{id}[/]', \lbs\command\control\CommandesController::class.':getCommand')->setName('commande_api');
-$app->post('/contacts/{nom}[/]',function(Request $req, Response $resp, array $args) {
-    $commande_test = new commande();
-    $commande_test->id = "2222";
-    $commande_test->nom = $args['nom'];
-    $commande_test->livraison = "2019-11-10 13:05:56";
-    $commande_test->mail = "dede";
-    $commande_test->save();
-    $rs = $resp->withStatus(201)
-        ->withHeader('Location','http://api.commande.local:19080/commandes/'.$commande_test->id)
-        ->withHeader('Content-Type', 'application/json;charset=utf-8');
-    $rs->getBody()->write($sushi = commande::find($commande_test->id));
-    return $rs;
-});
 $app->run();
