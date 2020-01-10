eb272fec17ecee2b12bca668e46c5024b23a35dc
sehbaoui <mouad>
2020-01-10 13:02:06 +0100

    TD3

--- commande_api/src/control/CommandesController.php
+++ commande_api/src/control/CommandesController.php
@@ -6,37 +6,42 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use MongoDB\Driver\WriteError;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use \lbs\command\model\Commande as commande;
 
-class CommandesController {
-	protected $c;
+class CommandesController
+{
+    protected $c;
 
-	public function __construct( \Slim\Container $c = null){
-		$this->c = $c;
-	}
+    public function __construct(\Slim\Container $c = null)
+    {
+        $this->c = $c;
+    }
 
-	public function getCommands(Request $req, Response $resp, array $args) {
+    public function getCommands(Request $req, Response $resp, array $args)
+    {
 
         try {
 
-	    $cde = \lbs\command\model\Commande::all();
+            $cde = \lbs\command\model\Commande::all();
 
-		$rs = $resp->withStatus(200)
-					->withHeader('Content-Type', 'application/json;charset=utf-8');
+            $rs = $resp->withStatus(200)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8');
 
-		$rs->getBody()->write(json_encode([
-					"type" => "collection",
-					"count" => $cde,
-					"commandes" => $cde]));
+            $rs->getBody()->write(json_encode([
+                "type" => "collection",
+                "count" => $cde,
+                "commandes" => $cde]));
 
             return $rs;
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             echo "HOla";
             return Writer::json_error($rs, 404, $e->getMessage());
         }
 
-	}
+    }
 
-	public function getCommand(Request $req, Response $resp, array $args) {
+    public function getCommand(Request $req, Response $resp, array $args)
+    {
         try {
             $id = $args['id'];
 
@@ -51,13 +56,69 @@ class CommandesController {
 
             return $rs;
 
-        }catch(ModelNotFoundException $e){
+        } catch (ModelNotFoundException $e) {
 
             $rs = $resp->withStatus(404)
                 ->withHeader('Content-Type', 'application/json;charset=utf-8');
-            $rs->getBody()->write(json_encode(['Error_code'=>404, 'Error message'=>$e->getMessage()]));
+            $rs->getBody()->write(json_encode(['Error_code' => 404, 'Error message' => $e->getMessage()]));
 
             return $rs;
         }
-	}
+    }
+
+    public function insertCommand(Request $req, Response $resp, array $args)
+    {
+        try {
+            if (filter_var($args['mail'], FILTER_VALIDATE_EMAIL) == !0) {
+                $commande_test = new commande();
+                $commande_test->id = uniqid();
+                $commande_test->nom = (filter_var($args['nom'], FILTER_SANITIZE_STRING));
+                $commande_test->livraison = date("Y-m-d h:i:s");
+                $commande_test->mail = filter_var($args['mail'], FILTER_VALIDATE_EMAIL);
+                $commande_test->save();
+                $rs = $resp->withStatus(201)
+                    ->withHeader('Location', 'http://api.commande.local:19080/commandes/' . $commande_test->id)
+                    ->withHeader('Content-Type', 'application/json;charset=utf-8');
+                $rs->getBody()->write(json_encode(commande::find($commande_test->id)));
+                return $rs;
+            } else {
+                echo "please insert a valid email address";
+            }
+        } catch (ModelNotFoundException $e) {
+            $rs = $resp->withStatus(404)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8');
+            $rs->getBody()->write(json_encode(['Error_code' => 404, 'Error message' => $e->getMessage()]));
+            return $rs;
+        }
+    }
+
+
+    public function updateCommand(Request $req, Response $resp, array $args)
+    {
+        if ($commande_test = commande::find($args["id"])) {
+            switch ($args["data"]) {
+                case "mail":
+                    if (filter_var($args['value'], FILTER_VALIDATE_EMAIL) == !0) {
+                        $commande_test->mail = filter_var($args['value'], FILTER_VALIDATE_EMAIL);
+                        $commande_test->save();
+                    } else {
+                        echo "please use a valid email";
+                    }
+                    break;
+                case "nom":
+                    $commande_test->nom = filter_var($args['value'], FILTER_SANITIZE_STRING);
+                    $commande_test->save();
+                    break;
+            }
+            $rs = $resp->withStatus(200)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8');
+            $rs->getBody()->write(json_encode($commande_test));
+            return $rs;
+        } else {
+            $rs = $resp->withStatus(404)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8');
+            $rs->getBody()->write(json_encode(['Error_code' => 404, 'please enter an existing id']));
+            return $rs;
+        }
+    }
 }
