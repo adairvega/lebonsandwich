bc3e7e225f3e5882f3efa8d3ca04cafebfff58ff
sehbaoui <mouad>
2020-01-10 13:02:06 +0100

    TD3 half-through

--- commande_api/src/control/CommandesController.php
+++ commande_api/src/control/CommandesController.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use MongoDB\Driver\WriteError;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use \lbs\command\model\Commande as commande;
 
 class CommandesController {
 	protected $c;
@@ -60,4 +61,29 @@ class CommandesController {
             return $rs;
         }
 	}
+
+    public function insertCommand(Request $req, Response $resp, array $args) {
+        try {
+            if (filter_var($args['mail'], FILTER_VALIDATE_EMAIL) ==! 0){
+                $commande_test = new commande();
+                $commande_test->id = uniqid();
+                $commande_test->nom = (filter_var($args['nom'],FILTER_SANITIZE_STRING));
+                $commande_test->livraison = date("Y-m-d h:i:s");
+                $commande_test->mail = filter_var($args['mail'], FILTER_VALIDATE_EMAIL);
+                $commande_test->save();
+                $rs = $resp->withStatus(201)
+                    ->withHeader('Location','http://api.commande.local:19080/commandes/'.$commande_test->id)
+                    ->withHeader('Content-Type', 'application/json;charset=utf-8');
+                $rs->getBody()->write(json_encode(commande::find($commande_test->id)));
+                return $rs;
+            }else{
+                echo "please insert a valid email address";
+            }
+        }catch(ModelNotFoundException $e){
+            $rs = $resp->withStatus(404)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8');
+            $rs->getBody()->write(json_encode(['Error_code'=>404, 'Error message'=>$e->getMessage()]));
+            return $rs;
+        }
+    }
 }
