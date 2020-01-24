0c98a0904047f26e5f97873a280c35c76296ffc7
sehbaoui <mouad>
2020-01-24 21:07:48 +0100

    TD5 1

--- catalogue_api/api/index.php
+++ catalogue_api/api/index.php
@@ -6,8 +6,6 @@ $errors = require './conf/errors.php';
 $configuration = new \Slim\Container(['settings' => ['displayErrorDetails' => true]]);
 $app_config = array_merge($errors);
 $app = new \Slim\App(new \Slim\Container($app_config));
-$c = new \MongoDB\Client("mongodb://dbcat");
-
-$app->get('/categories[/]', \lbs\command\control\CategoriesController::class . ':getCategorie');
+$app->get('/categories/{id}/{sandwichs}', \lbs\command\control\CategoriesController::class . ':getCategorie');
 
 $app->run();
