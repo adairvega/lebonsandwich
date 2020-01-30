2dc0af60d71b1b2d5ba9d0a0ffe08a21cb29d4f3
sehbaoui <mouad>
2020-01-30 14:18:59 +0100

    jonathan import

--- checkcommande_api/src/control/pointVenteController.php
+++ checkcommande_api/src/control/pointVenteController.php
@@ -36,7 +36,7 @@ class pointVenteController
             $order["items"] = $items;
             $links = array(
                 "self" => "http://api.checkcommande.local:19280/commandes/" . $id,
-                "items" => "http://api.pointvente.local:19380/commands/" . $id . "/"
+                "items" => "http://api.checkcommande.local:19280/commands/" . $id . "/items"
             );
             $rs = $resp->withStatus(200)
                 ->withHeader('Content-Type', 'application/json;charset=utf-8');
@@ -44,6 +44,29 @@ class pointVenteController
                 "type" => "resource",
                 "links" => $links,
                 "command" => $order]));
+            return $rs;
+        } catch (ModelNotFoundException $e) {
+            $rs = $resp->withStatus(404)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8');
+            $rs->getBody()->write(json_encode(['Error_code' => 404, 'Error message' => $e->getMessage()]));
+            return $rs;
+        }
+    }
+
+
+    public function getItems(Request $req, Response $resp, array $args)
+    {
+        try {
+            $id = $args['id'];
+            $cde = Commande::findOrFail($id);
+            $items = $cde->commandeItems()->get();
+
+            $rs = $resp->withStatus(200)->withHeader('Content-Type', 'application/json;charset=utf-8');
+            $rs->getBody()->write(json_encode([
+                "type" => "item",
+                "id" => $id,
+                "items" => $items
+            ]));
             return $rs;
         } catch (ModelNotFoundException $e) {
             $rs = $resp->withStatus(404)
